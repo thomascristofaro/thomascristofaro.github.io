24e879a8856c30db0535c859b4c2d5684fb8222c
Thomas Cristofaro <cristofaro.thomas@gmail.com>
2017-09-27 17:31:16 +0200

    icone, centramento content e prima versione della home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,13 @@
-/*$(function() {
 
-  $(".row").height($(window).height());
-
-  // Init ScrollMagic
-  var controller = new ScrollMagic.Controller();
-
-  $(".row").each(function() {
-    new ScrollMagic.Scene({
-        triggerElement: this,
-        triggerHook: 'onLeave'
-      })
-      .setPin(this)
-      .addTo(controller);
-  });
-
-});*/
-
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+particlesJS.load('particles-js', 'assets/particles.json');
 
 $(function() {
-  var h = $(window).height();
-  $("section").height(h);
-  //$("canvas").attr("height", h);
-  $("#particles-js").height(h-10);
+  $(window).resize(height);
+  height();
+
+  //margin-top per centrare il contenitore
+  var margin = ($(window).height()/2) - ($("#home").height()/2);
+  $("#home").css("margin-top", margin);
 
   // Init ScrollMagic
   var controller = new ScrollMagic.Controller();
@@ -38,3 +21,11 @@ $(function() {
       .addTo(controller);
   });
 });
+
+function height() {
+     var h = $(window).height();
+     //var h = screen.height;
+     $("section").height(h);
+     $("#particles-js").height(h);
+     $("canvas").attr("height", h);
+}
